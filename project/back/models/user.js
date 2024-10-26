e00678d8bbd7a5f66e9a24d58fd0fe0a5729c084
Faysoula <faysaljunior.elestwani@gmail.com>
2024-10-26 23:42:50 +0300

    user model added

---
+++ project/back/models/user.js
@@ -0,0 +1,45 @@
+const { DataTypes } = require("sequelize");
+const { db } = require("../config/db");
+
+const User = db.define("User", {
+  user_id: {
+    type: DataTypes.INTEGER,
+    primaryKey: true,
+    autoIncrement: true,
+  },
+
+  username: {
+    type: DataTypes.STRING(50),
+    allowNull: false,
+  },
+
+  email: {
+    type: DataTypes.STRING(100),
+    allowNull: false,
+    validate: {
+      isEmail: true,
+    },
+  },
+
+  password: {
+    type: DataTypes.STRING(255),
+    allowNull: false,
+  },
+  name: {
+    type: DataTypes.STRING(50),
+    allowNull: true,
+  },
+  last_name: {
+    type: DataTypes.STRING(50),
+    allowNull: true,
+  },
+  created_at: {
+    type: DataTypes.DATE,
+    defaultValue: DataTypes.NOW,
+  },
+ }, {
+    tableName: "users",
+    timestamps: false,//alr have them in the columns
+});
+
+module.exports = User;
