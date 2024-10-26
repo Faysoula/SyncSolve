ad81b6bd6ab31ab5dced0d1d56df3ff95d876215
Faysoula <faysaljunior.elestwani@gmail.com>
2024-10-26 21:57:04 +0300

    DB CONFIG ADDED

---
+++ project/back/config/db.js
@@ -0,0 +1,26 @@
+const { Sequelize } = require('sequelize');
+require('dotenv').config();
+
+const db = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASSWORD, {
+    host: process.env.DB_HOST,
+    port: process.env.DB_PORT,
+    dialect: process.env.DB_DIALECT,
+    logging: false,
+});
+
+async function testConnection() {
+    try {
+        await db.authenticate();
+        console.log('Connection has been established successfully.');
+    } catch (error) {
+        console.error('Unable to connect to the database:', error);
+        console.log("DB_NAME:", process.env.DB_NAME);
+        console.log("DB_USER:", process.env.DB_USER);
+        console.log("DB_PASS:", process.env.DB_PASS); // Check if this logs correctly as a string
+        console.log("DB_HOST:", process.env.DB_HOST);
+        console.log("DB_PORT:", process.env.DB_PORT);
+        console.log("DB_DIALECT:", process.env.DB_DIALECT);
+    }
+}
+
+module.exports = { db, testConnection };
---
+++ project/back/index.js
@@ -0,0 +1,30 @@
+const express = require('express');
+const cors = require('cors');
+const bodyParser = require('body-parser');
+const { testConnection } = require('./config/db');
+require('dotenv').config();
+
+const app = express();
+
+const PORT = process.env.PORT || 5000;
+
+app.use(cors());
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.get("/", (req, res) => {
+  res.send("api running yay");
+});
+
+app.use((err, req, res, next) => {
+  console.error(err.stack);
+  res.status(500).send("Something broke!");
+});
+
+
+app.listen(PORT, () => {
+  console.log(`====================================`);
+  console.log(`Server running on port ${PORT}`);
+  testConnection();
+  console.log(`====================================`);
+});
